a840065e3d4534612da1e7a237b3386080609cfe
user <user@users-MacBook-Pro.local>
2022-10-27 01:08:18 +0100

    push

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-selection'>
+      <h1 className='section-title'>
+        about us
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, 
+        perferendis! Atque nihil alias fuga, exercitationem animi nostrum ad, 
+        doloribus neque nisi enim facere expedita dolorum hic quam quos quasi eveniet! 
+        Maiores tenetur obcaecati nulla harum dicta. Distinctio laboriosam architecto blanditiis.
+      </p>
+    </section>
   )
 }
 
